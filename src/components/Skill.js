c16a074d1b1c74dfc145aca539a8052adf96f56f
David Delgado <david.delgadop88@gmail.com>
2023-08-24 16:56:07 -0400

    challenge 2: list rendering & conditional content

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,7 +1,17 @@
 const Skill = (props) => {
+    
+    const level = props.level;
+    let emoji = '💪';
+
+    if (level === 'beginner') {
+        emoji = '👶';
+    } else if (level === 'intermediate') {
+        emoji = '👍';
+    }
+
     return (
         <div className="skill" style={{backgroundColor: `${props.color}`}}>
-            <h3 className="skill-title">{props.title}</h3>
+            <h3 className="skill-title">{props.title} {emoji}</h3>
         </div>
     );
 }
--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,28 +1,51 @@
 import Skill from "./Skill";
 
+//   
+
+const skills = [
+    {
+      title: "HTML",
+      level: "advanced",
+      color: "#d17f0a",
+      id: 0
+    },
+    {
+      title: "CSS",
+      level: "advanced",
+      color: "#0f5fdf",
+      id: 1
+    },
+    {
+      title: "JavaScript",
+      level: "intermediate",
+      color: "#e1c202",
+      id: 2
+    },
+    {
+      title: "React",
+      level: "beginner",
+      color: "#521c81",
+      id: 3
+    },
+    {
+      title: "C++",
+      level: "intermediate",
+      color: "#193773",
+      id: 4
+    }
+];
+
 const SkillList = () => {
     return (
         <div className="skills">
-            <Skill 
-                color='#d17f0a'
-                title='HTML'
-            />
-            <Skill 
-                color='#0f5fdf'
-                title='CSS'
-            /> 
-            <Skill
-                color='#e1c202'
-                title='JavaScript'
-            />
-            <Skill
-                color='#193773'
-                title='C++'
-            />
-            <Skill
-                color='#521c81'
-                title='React'
-            />
+            {skills.map((skill) => (
+                <Skill 
+                    key={skill.id}
+                    color={skill.color}
+                    title={skill.title}
+                    level={skill.level}
+                />
+            ))}
         </div>
     );
 };
